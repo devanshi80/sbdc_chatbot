bff276e8761cb31abfab5912d55c0e798e361f5d
devanshi80 <136477608+devanshi80@users.noreply.github.com>
2025-10-07 15:27:55 -0500

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,14 @@ class Config:
         self.questions_path = os.path.join(base_path, "questions.json")
         self.tone_matrix_path = os.path.join(base_path, "tone.json")
         self.rules_path = os.path.join(base_path, "rules.json")  
+        self.catalyst_path = os.path.join(base_path, "catalyst.json")
+        self.functional_path = os.path.join(base_path, "functional_area.json") 
 
         self.questions = self._load_json(self.questions_path)
         self.tone_matrix = self._load_json(self.tone_matrix_path)
         self.rules = self._load_json(self.rules_path)
+        self.catalysts = self._load_json(self.catalyst_path)
+        self.functional_areas = self._load_json(self.functional_path)
 
     def _load_json(self, path):
         if not os.path.exists(path):
